8e16b81f1f41c70b20d263f2dc383241323117a5
ldinc <drogunov.igor@gmail.com>
2017-04-24 00:54:07 +0300

    added skeleton project

---
+++ AutocompleteWPF/Autocomplete.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutocompleteWPF {
+  /// <summary>
+  /// Interaction logic for Autocomplete.xaml
+  /// </summary>
+  public partial class Autocomplete : UserControl {
+    public Autocomplete() {
+      InitializeComponent();
+    }
+  }
+}
